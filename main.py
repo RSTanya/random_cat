cc1352daa36c21770e527534d44d67a6176f5495
RSTanya <rezanovatanyaa@gmail.com>
2024-08-15 17:43:43 +0400

    add bootstrap

--- main.py
+++ main.py
@@ -6,14 +6,25 @@ import json
 app = Flask(__name__)
 
 
+def get_cat():
+    cat = {}
+    while not cat:
+        # print("hi")
+        response = requests.get("https://api.thecatapi.com/v1/images/search")
+        str_result = response.text
+        result = json.loads(str_result)
+        if result[0]["width"] <= 1000 and result[0]["height"] <= 1000:
+            cat = result[0]
+    return cat
+
+
 @app.route('/')
 def index():
-    response = requests.get("https://api.thecatapi.com/v1/images/search")
-    str_result = response.text
-    result = json.loads(str_result)
-
-    return render_template('index.html', cats=result[0])
+    cat = get_cat()
+    return render_template('index.html', cats=cat)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+print(get_cat())
